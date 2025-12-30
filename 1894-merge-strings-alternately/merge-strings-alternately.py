70fe04a8d1921af268251adafeddba4bb487d5e3
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2025-12-30 17:20:26 +0530

    Time: 39 ms (45.94%) | Memory: 17.2 MB (97.30%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -3,15 +3,14 @@ class Solution:
         s=""
         l1=len(word1)
         l2=len(word2)
-        i,j=0,0
-        while i<l1 and j<l2:
+        i=0
+        while i<l1 and i<l2:
             s+=word1[i]
-            s+=word2[j]
+            s+=word2[i]
             i+=1
-            j+=1
-        if i>l1-1 and j<=l2-1 :
-            s+=word2[j:]
-        elif j>l2-1 and i<=l1-1 :
+        if i>l1-1 and i<=l2-1 :
+            s+=word2[i:]
+        elif i>l2-1 and i<=l1-1 :
             s+=word1[i:]
 
         return s
