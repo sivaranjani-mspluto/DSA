4b6b9374c8bfa4208a0d86059cb8260813adfcf2
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-01-05 21:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (73.11%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       hmap={}
+       for i,cnum in enumerate(nums):
+        if target-cnum in hmap:
+            return [i,hmap[target-cnum]]
+        else:
+            hmap[cnum] =i
+
+
